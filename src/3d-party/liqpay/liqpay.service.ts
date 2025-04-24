a092b5466eafd34c96b6a5880214c3fce3711a8e
admin <admin@admins-MacBook-Pro.local>
2025-04-24 17:42:50 +0300

    fix: fix date liqpay

--- src/3d-party/liqpay/liqpay.service.ts
+++ src/3d-party/liqpay/liqpay.service.ts
@@ -50,7 +50,7 @@ export class LiqpayService {
     }
 
     const now = new Date();
-    const formattedDate = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
+    const formattedDate = `${now.getFullYear() + 1}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
 
     const self = this;
 
